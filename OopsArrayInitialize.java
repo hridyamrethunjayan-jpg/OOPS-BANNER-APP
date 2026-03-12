aff3c83850e5983d593e9ec1116aa64d822c1c58
hridyamrethunjayan-jpg <hridyamrethunjayan@gmail.com>
2026-03-12 09:49:28 +0530

    uc7

--- OopsArrayInitialize.java
+++ OopsArrayInitialize.java
@@ -1,15 +1,15 @@
 public class OopsArrayInitialize{
     public static void main(String[] args){
         String[] lines={
-        String.join(" ","  ******  ","  *****  ","******** "," ******** "),
+        String.join(" ","  *****   ","  *****  ","******** "," ******** "),
         String.join(" "," *     * "," *     * "," *       *"," *        "),
         String.join(" ","*       *","*       *"," *       *"," *        "),
         String.join(" ","*       *","*       *"," ******** "," ******** "),
         String.join(" ","*       *","*       *"," *        ","        * "),
         String.join(" "," *     * "," *     * "," *        ","        * "),
-        String.join(" "," *****   ","  *****  "," *        "," ******** ")
+        String.join(" ","  *****   "," *****  "," *        "," ******** ")
         };
-        
+
         for(String line:lines){
             System.out.println(line);
         }
